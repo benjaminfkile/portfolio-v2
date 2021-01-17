c81d81709ca2e48050341a613ac83be6df6f5889
ben <benjaminfkile@gmail.com>
2021-01-16 23:09:40 -0700

    switch back to colored cube icons

--- src/Home/Cube/Cube.js
+++ src/Home/Cube/Cube.js
@@ -7,22 +7,22 @@ class Cube extends Component {
             <div className="cube-container">
                 <div id="cube">
                     <figure className="face front">
-                        <i className="devicon-java-plain-wordmark"></i>
+                        <i className="devicon-java-plain-wordmark colored"></i>
                     </figure>
                     <figure className="face back">
-                        <i className="devicon-heroku-original-wordmark"></i>
+                        <i className="devicon-heroku-original-wordmark colored"></i>
                     </figure>
                     <figure className="face left">
-                    <i className="devicon-webpack-plain-wordmark"></i>
+                    <i className="devicon-git-plain-wordmark colored"></i>
                     </figure>
                     <figure className="face right">
-                        <i className="devicon-react-original-wordmark"></i>
+                        <i className="devicon-react-original-wordmark colored"></i>
                     </figure>
                     <figure className="face top">
-                    <i className="devicon-nodejs-plain-wordmark"></i>
+                    <i className="devicon-nodejs-plain colored"></i>
                     </figure>
                     <figure className="face bottom">
-                        <i className="devicon-postgresql-plain-wordmark"></i>
+                        <i className="devicon-postgresql-plain-wordmark colored"></i>
                     </figure>
                 </div>
             </div>
